39fe8528d9561753fcd01976b63837ae1ffdecb3
LSYSmallFish <332547960@qq.com>
2020-03-23 10:59:30 +0800

    Initia seven

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static' ## 新增行
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, '/static/'), ##修改地方
+    os.path.join(BASE_DIR, "static")
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 PAGINATION_SETTINGS = {
