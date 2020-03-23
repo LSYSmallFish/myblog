5101dcf5d0efd5ae823cbbd9165a2d8ee835b684
LSYSmallFish <332547960@qq.com>
2020-03-23 11:05:01 +0800

    Initia eght

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "/static/")
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 PAGINATION_SETTINGS = {
